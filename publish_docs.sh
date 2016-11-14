a91829cc8cadd332920526126d8b0882311e51cf
Tres Wong-Godfrey <tres@clean.io>
2016-11-14 10:57:49 -0800

    ReCodify Making Docs
             COT   cot COOOT!
         cot ,,    ___
       COT'.>' \_/'--.'
            "( (( -'/<
              \  _- /
           mrf )/ )/
             -((-((---

---
+++ publish_docs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mv output /tmp/output/ 
+
+git checkout master
+
+php phpDocumentor.phar -d src
+
+git checkout gh-pages
+
+cd output
+
+for i in `ls`
+do
+    mv ../$i /tmp/$i
+    mv $i ../$i
+done
+
+cd -
+
+git add classes files
+
+git commit -a -m "Documentation Update"
+
+git checkout master
