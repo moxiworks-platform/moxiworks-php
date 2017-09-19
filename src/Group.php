390cf9a249b6901df4b3edbc749efae5f5a0a5a0
Tres Wong-Godfrey <tres@clean.io>
2017-09-19 13:38:23 -0700

    Mod: Update Object Response To Reflect Service Response Data
    fixes #1
    
                _..----''"`""'"'"`--.._
         __...+"                       `-.
       ,'--.                              `.
         ,'                                 '.
        .                                     .
       .             .-'/                      `
       :  .        .'  ,  ,                     .
       : ,  .     '   .  ,    ,                 .
       '.  .     '    : .:   .    .             :
       .: ,     :     ' :`   :.   :             :
        ' :     :     `.: `. : .  :             :
         `.`  '  `"".   `  ,`". `.`.            .
           ``. `.|  ^.        ,`  `.`.          ;          _,
             ,'  : (`;       ( ":     :         '       ,-" /_
            :    :`+' ,    `._>.'     :        '      .'   '  `.
            `.   :   `.            ."-:       :     .'          .
              :  :   ----,        ,.`..`.     '    .             .
              :  `.  `. /         ... `. `.    `..'              :
              '    .   `        ,' ..`. .  `.         _.-""-.    :
              `.    `.       _.'    .. . `.  .     _."       `. ,'
                `.    `._..,<        `.    ,  ` _.'            `---.__
              __.'           `.     ,' .  .    `                 .'   `"-._
              `-...      __./`   _._...`  :     `--.....,....,,-'"""""-..  `.
                  :    ."  :  `.' (__     `                                  `.
                  `.  ,    | ."'      `-._____,..___                        `  .
                    `. .   | ' .`.    ."        `-. `"-._                      :
                     : .   |'  ,  . ,'             `._   `-._                  ,
                     '  .     ,`   `                  `-._  :`-._           _.'
                      \ .'  ,'  .                   ,'    `.l    `",..___..'
                      ,'  .'    :                 .        .`"-..-'
                     .   '     .:                '        ,
                    ,   .     ' |               '        .
                   .    :    '  ;              ,       .',
                   :    `   :   .             .      ," .
                   |     .  :   '          .',     .' ,  .
                   '      .  .  `     .   ' .       .    '
                    .      .  .  `  .'    ."  _.-',     '
                     `._    `. >..`, .' .'  .'   .       `._
                        `-- . j   :.' .'"`'`.  .'     __.'  '
                          `. ,^   ; .'`-..---`"`"._    ,   /
                           .'  `""`  .             `.     '`.
                          ,    :`. : .`              `. .'   `._
                         .     ;  `:. ``.              `        `-.__
                          .'   :   ;:    `.                          `"--._
                         , ,   .   :.     '                                `.
                         `,    :   ;.    /`.  `                              `.
                          '    |   `: . .  :`.   `+.                           `.
                         .    ,'    ' .,   :  `                                  .
                         :  .'       `'    :   `                                  .
                          `"               :    `                                  .

--- src/Group.php
+++ src/Group.php
@@ -29,7 +29,6 @@ class Group extends Resource
      */
     public $agent_uuid;
 
-
     /**
      * @var string the name of the Group on the Moxi Works Platform
      *
@@ -37,12 +36,48 @@ class Group extends Resource
     public $moxi_works_group_name;
 
     /**
+     * @var string the ID of the Group on the Moxi Works Platform
+     *
+     */
+    public $moxi_works_group_id;
+
+    /**
+     * @var boolean whether the group ID continues to exist beyond a name change.
+     * Some providers (namely versions of Exchange) provide no permanent unique
+     * identifier for groups. Group IDs for these providers are inherently transient.
+     * Moxi Works has no way to guarantee that any Group marked transient will
+     * persist since the name can be changed outside of The Moxi Works platform
+     * and Exchange provides no persistent IDs for their Group entities.
+     *
+     */
+    public $transient;
+
+    /**
      * @var array an array filled with MoxiworksPlatform\Contact objects representing
      *      all the Contacts in the specified MoxiworksPlatform\Group
+     *
+     *  This attribute will only be in find responses
      *
      */
     public $contacts;
 
+    /**
+     * @var $page_number integer use this attribute for paging if the number of
+     * contacts spans multiple response pages
+     *
+     *  This attribute will only be in find responses
+     *
+     */
+    public $page_number;
+
+    /**
+     * @var $total_pages integer use this attribute for paging if the number of
+     * contacts spans multiple response pages
+     *
+     *  This attribute will only be in find responses
+     *
+     */
+    public $total_pages;
 
     /**
      * Group constructor.
@@ -60,14 +95,16 @@ class Group extends Resource
     /**
      * Find a specified Group by name on the Moxi Works Platform.
      *
-     * find can be performed including the Moxi Works Group name and the Moxi Works Agent ID in a parameter array
+     * find can be performed including the Moxi Works Group ID and the Moxi Works Agent ID in a parameter array
+     *
+     *  use MoxiworksPlatform\Group::search to determine what group IDs are available.
      *  <code>
-     *  \MoxiworksPlatform\Group::find([moxi_works_agent_id: 'abc123', moxi_works_group_name: 'groupName'])
+     *  \MoxiworksPlatform\Group::find([moxi_works_agent_id: 'abc123', moxi_works_group_id: 'groupId'])
      *  </code>
      * @param array $attributes
      *       <br><b>moxi_works_agent_id *either agent_uuid or moxi_works_agent_id are REQUIRED* </b>The Moxi Works Agent ID for the agent to which this group is to be associated
      *       <br><b>agent_uuid *either agent_uuid or moxi_works_agent_id are REQUIRED* </b>The Moxi Works Agent UUID for the agent to which this group is to be associated
-     *       <br><b>moxi_works_group_name *REQUIRED* </b>The Moxi Works Group Name for this Group
+     *       <br><b>moxi_works_group_id *REQUIRED* </b>The Moxi Works Group Name for this Group
      *
      *
      * @return Group|null
@@ -75,10 +112,15 @@ class Group extends Resource
      * @throws ArgumentException if required parameters are not included
      * @throws RemoteRequestFailureException
      */
-    public static function find($attributes = [])
+    public static function find($opts = [])
     {
+        $required_opts = array('moxi_works_group_id');
+        if(count(array_intersect(array_keys($opts), $required_opts)) != count($required_opts))
+            throw new ArgumentException(implode(',', $required_opts) . " are required");
+
         $contacts = array();
-        $group = Group::sendRequest('GET', $attributes);
+        $url = Config::getUrl() . "/api/groups/" . $opts['moxi_works_group_id'];
+        $group = Group::sendRequest('GET', $opts, $url);
         
         if(is_null($group) || empty($group))
             return null;
@@ -110,8 +152,7 @@ class Group extends Resource
      * @throws ArgumentException if at least one search parameter is not defined
      * @throws RemoteRequestFailureException
      */
-    public static function search($attributes = [])
-    {
+    public static function search($attributes = []) {
         $method = 'GET';
         $url = Config::getUrl() . "/api/groups";
         $results = array();
@@ -123,6 +164,16 @@ class Group extends Resource
 
         foreach ($json as $element) {
             $group = new Group($element);
+            $group->moxi_works_agent_id = $attributes["moxi_works_agent_id"];
+            $group->agent_uuid = $attributes["agent_uuid"];
+            unset($group->total_pages);
+            unset($group->page_number);
+            unset($group->contacts);
+            if($group->moxi_works_agent_id == null) {
+                unset($group->moxi_works_agent_id);
+            } else if($group->agent_uuid == null) {
+                unset($group->agent_uuid);
+            }
             array_push($results, $group);
         }
         return $results;
@@ -143,9 +194,6 @@ class Group extends Resource
         if($url == null) {
             $url = Config::getUrl() . "/api/groups";
         }
-        $required_opts = array('moxi_works_group_name');
-        if(count(array_intersect(array_keys($opts), $required_opts)) != count($required_opts))
-            throw new ArgumentException(implode(',', $required_opts) . " are required");
         $group = null;
         $json = Resource::apiConnection($method, $url, $opts);
         $group = (!isset($json) || empty($json)) ? null : new Group($json);
